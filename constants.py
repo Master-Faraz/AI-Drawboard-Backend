3793aa5e3f70bc3694a50a8968dcf97376243ca5
Faraz Ali <farazali9028@gmail.com>
2024-09-22 14:59:07 +0530

    test

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-SERVER_URL = 'https://ai-drawboard-backend.onrender.com'
+# SERVER_URL = 'https://ai-drawboard-backend.onrender.com'
 # PORT = '8000'
 # ENV = 'dev'
 
